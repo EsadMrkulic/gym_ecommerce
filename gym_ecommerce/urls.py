dc7e199c312a1d401f44443d7f97dca6b4fbc7b1
Esad Mrkulic <esadmrkulic125@gmail.com>
2024-02-03 12:27:15 -0800

    Created Models and added more settings

--- gym_ecommerce/urls.py
+++ gym_ecommerce/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),  # Including a new URL that leads to the store app
-]
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)  # To allow image upload
--- store/models.py
+++ store/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+# Model for categories of products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    # Method to allow the model to appear in the Django admin panel
+    def __str__(self):
+        return self.name
+
+
+# Customer model
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    # Show the model in admin panel
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# All products model
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, default=1
+    )  # Foreign Key references this category value to the class Category
+    description = models.CharField(max_length=250, default="", blank=True, null=True)
+    image = models.ImageField(upload_to="uploads/product/")
+
+    # Show the model in admin panel
+    def __str__(self):
+        return self.name
+
+
+# Customer orders model
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default="", blank=True)
+    phone = models.CharField(max_length=15, default="", blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    # Show the model in admin panel
+    def __str__(self):
+        return self.product
