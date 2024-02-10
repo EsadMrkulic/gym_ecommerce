89f55ad31032f14e62ff10768a91b122fc9a22b9
Esad Mrkulic <esadmrkulic125@gmail.com>
2024-02-10 11:44:50 -0800

    added shopping cart messages

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -44,22 +45,32 @@ def cart_add(request):
         # Return response
         # response = JsonResponse({"Product Name:": product.name})
         response = JsonResponse({"qty": cart_quantity})
+        messages.success(
+            request, (product.name + " has been added to your shopping cart!")
+        )
         return response
 
 
 # Deleting from cart view
 def cart_delete(request):
     cart = Cart(request)
+
     # Using part of the code from cart_add view
     if request.POST.get("action") == "post":
 
         # Get id and quantity of product
         product_id = int(request.POST.get("product_id"))
 
+        # Retrieve product instance for the success message
+        product = get_object_or_404(Product, id=product_id)
+
         # Call delete function in cart
         cart.delete(product=product_id)
-
+        messages.success(
+            request, (product.name + " has been removed from your shopping cart!")
+        )
         response = JsonResponse({"product": product_id})
+
         return response
 
 
@@ -76,5 +87,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({"qty": product_qty})
+        messages.success(request, ("Your cart has been updated!"))
         return response
         # return redirect('cart_summary')
